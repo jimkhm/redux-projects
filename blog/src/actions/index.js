8c771d2c8dbbd9397cb118cbd351f465925e99a9
magna <magnking@gmail.com>
2017-08-23 15:45:40 +0900

    add cancel botton and ajax post feature

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 export const FETCH_POSTS = 'fetch_posts';
+export const CREATE_POST = 'create_post';
 
-const ROOT_URL = '	http://reduxblog.herokuapp.com/api'
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api'
 const API_KEY= '?key=PAPERCLIP0123456';
 
 export function fetchPosts() {
@@ -11,4 +12,14 @@ export function fetchPosts() {
     type: FETCH_POSTS,
     payload: request
   };
+}
+
+export function createPost(values) {
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values);
+
+
+  return {
+    type: CREATE_POST,
+    payload: request
+  }
 }
